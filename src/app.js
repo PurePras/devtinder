ef1f5561f291ffa73c665c81a4f37b028966f19d
PurePras <prashantmanhar2001@gmail.com>
2025-09-19 13:29:45 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -3,28 +3,32 @@ const app = express();
 // below is called request handler function
 // app .use match all http match
 // .get only handle GET calls
-app.use("/user",(req,res)=>{
-    res.send("hahahahhahah");
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log("query is : ",req.params);
+    res.send("ab path regex");
 })
-app.get("/user",(req,res)=>{
-    res.send("path get testing")
-})
-app.post("/user",(req,res)=>{
-    console.log("data is saved in db");
-    res.send("saved post successfully");
-})
-app.delete("/user",(req,res)=>{
-    res.send("deleted successfully");
-})
-app.use("/test" ,(req,res)=>{
-    res.send("ello from the server");
-})
-app.use("/hello/12",(req,res)=>{
-    res.send("12 hello inside"); 
-}) 
-app.use("/hello",(req,res)=>{
- res.send("hello ") 
-})
+// app.use("/user",(req,res)=>{
+//     res.send("hahahahhahah");
+// })
+// app.get("/user",(req,res)=>{
+//     res.send("path get testing")
+// app.post("/user",(req,res)=>{
+//     console.log("data is saved in db");
+//     res.send("saved post successfully");
+// })
+// app.delete("/user",(req,res)=>{
+//     res.send("deleted successfully");
+// })
+// app.use("/test" ,(req,res)=>{
+//     res.send("ello from the server");
+// })
+// app.use("/hello/12",(req,res)=>{
+//     res.send("12 hello inside"); 
+// }) 
+// app.use("/hello",(req,res)=>{
+//  res.send("hello ") 
+// })
+
 // app.use("/",(req,res)=>{
 //     res.send("Namaste Prashant");
 // })
