2235629f288449f3e0692037065c1c94585137c0
PurePras <prashantmanhar2001@gmail.com>
2025-09-19 12:44:39 +0530

    exploring get,post,delete api on postman

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const app = express();
 // below is called request handler function
-app.use("/test" ,(req,res)=>{
-    res.send("hello from the server");
+// app .use match all http match
+// .get only handle GET calls
+app.use("/user",(req,res)=>{
+    res.send("hahahahhahah");
 })
-app.use("/hello",(req,res)=>{
- res.send("hello 1") 
+app.get("/user",(req,res)=>{
+    res.send("path get testing")
+})
+app.post("/user",(req,res)=>{
+    console.log("data is saved in db");
+    res.send("saved post successfully");
+})
+app.delete("/user",(req,res)=>{
+    res.send("deleted successfully");
+})
+app.use("/test" ,(req,res)=>{
+    res.send("ello from the server");
+})
+app.use("/hello/12",(req,res)=>{
+    res.send("12 hello inside"); 
 }) 
-app.listen(7777,()=>{
+app.use("/hello",(req,res)=>{
+ res.send("hello ") 
+})
+// app.use("/",(req,res)=>{
+//     res.send("Namaste Prashant");
+// })
+app.listen(7777 ,()=>{
     console.log("server is successfully listening on port 7777");
 });
--- src/homework.md
+++ src/homework.md
@@ -6,3 +6,13 @@ create a server
 listen to port 8888
 write request handlers for /test,/hello
 install nodemon and update scripts inside package.json
+-g while npm i
+diff between caret and tilde (^ vs ~)
+initialize git
+gitignore
+create a remote repo on github
+push all code to remote origin
+play with routes and route extensions ex. /hello, /, hello/12, /xyz
+order of routes matter a lot
+
+write loigc to handle get,post,patch,dlt,on postman
